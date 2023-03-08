bc48aa384c7a4972cc6ee9c648a928b944ffdebf
matheus <matheussss6613@gmail.com>
2023-03-08 18:54:02 -0300

    Criação de sapos

--- src/controller/SapoControll.java
+++ src/controller/SapoControll.java
@@ -3,15 +3,40 @@ package controller;
 public class SapoControll 
 {
 	
+	private String sapoNome;
+	private int identification;
+	private int metros = 0;
+	
+	public SapoControll(String nome, int num)
+	{
+		this.sapoNome = nome;
+		this.identification = num;
+		
+	}
+	
+	
+	public void sapoCorrida()
+	{
+		new Thread()
+		{
+			public void run() 
+			{
+				
+				
+				
+				
+				
+				
+			}
+		}.start();
+	}
 	
 	
 	
-	
-	
-	
-	
-	
-	
+	//cada sapo pula de 0m a 100m
+	//Distancia maxima é de 1000m
+	//Identificação do pulo de cada sapo
+	//Cada sapo tem uma classificação
 	
 	
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,21 @@
 package view;
 
+import controller.SapoControll;
+
 public class Main 
 {
 	public static void main(String[] args)
 	{
+		
+		SapoControll sapo1 = new SapoControll("Rodolfo", 1);	
+		SapoControll sapo2 = new SapoControll("Rodrigo", 2);		
+		SapoControll sapo3 = new SapoControll("Jessica", 3);		
+		SapoControll sapo4 = new SapoControll("Catharine", 4);		
+		SapoControll sapo5 = new SapoControll("Matheus", 5);		
+		
+		
+		
+		
 		
 		
 		
